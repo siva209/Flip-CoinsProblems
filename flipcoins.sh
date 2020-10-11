8c8433effdd5ad8c9e3044f3468c7313c6a11b1e
siva209 <sivanarayanayeruva209@gmail.com>
2020-10-11 21:12:34 +0530

    [flips program added by siva]

--- flipcoins.sh
+++ flipcoins.sh
@@ -1,1 +1,15 @@
-welcome flips
+#!/bin/bash -x
+headc=0
+tailc=0
+for ((i=0;i<20;i++))
+do
+var=$((RANDOM%2))
+if [ $var -eq 1 ]
+then 
+	((headc++))
+else
+	((tailc++))
+fi
+done
+echo "number of times head wins is $headc"
+echo "number of times tail wins is $tailc"
