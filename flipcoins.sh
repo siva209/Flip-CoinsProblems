0c2c3e33a3ac40fdf465fe9f98c1281f33cadc86
siva209 <sivanarayanayeruva209@gmail.com>
2020-10-11 21:20:02 +0530

    [number of times heads an tails win]

--- flipcoins.sh
+++ flipcoins.sh
@@ -1,15 +1,27 @@
 #!/bin/bash -x
 headc=0
 tailc=0
-for ((i=0;i<20;i++))
+diff=0
+while [ $headc -lt 21 -a $tailc -lt 21 ]
 do
 var=$((RANDOM%2))
-if [ $var -eq 1 ]
-then 
-	((headc++))
-else
-	((tailc++))
-fi
+	if [ $var -eq 1 ]
+	then 
+		((headc++))
+	else
+		((tailc++))
+	fi
 done
+	if [ $headc -eq 21 ]
+	then
+		diff=$(($headc - $tailc))
+		echo "head wins by $diff"
+	elif [ $tailc -eq 21 ]
+	then
+		diff=$(($tailc - $headc))
+		echo "Tail wins by $diff"
+	else
+		echo "no one wins"
+	fi
 echo "number of times head wins is $headc"
 echo "number of times tail wins is $tailc"
